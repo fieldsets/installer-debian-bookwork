b52cce1bbab7d1190078450c0f62be8694c67ef5
willgladstone <will.gl@dst.one>
2026-03-17 11:47:46 -0400

    fieldsets install script

--- src/install-fieldsets.sh
+++ src/install-fieldsets.sh
@@ -3,6 +3,6 @@ set -eEuo pipefail
 
 cd /usr/local/
 git clone --recurse-submodules https://github.com/fieldsets/fieldsets.git
-chown -R "${DEFAULT_ADMIN_USER}:${DEFAULT_ADMIN_GROUP}" /usr/local/fieldsets
+chown -R "${DEFAULT_ADMIN_USER}:${DEFAULT_ADMIN_USER}" /usr/local/fieldsets
 cd /usr/local/fieldsets
 cp ./env.example ./.env
